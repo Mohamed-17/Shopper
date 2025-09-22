78df188fc16267f22939ab81951c16e04cddbcfe
Mohamed ashraf <muhamed77700m@gmail.com>
2025-09-22 07:20:17 +0300

    add sanity & clerk to the project , now we have an API (CMS)!

--- components/CurrencyFormatter.tsx
+++ components/CurrencyFormatter.tsx
@@ -18,8 +18,12 @@ function CurrencyFormatter({
   const priceAfterDiscount = toUSCurrency(amount + (amount * discount) / 100);
   return (
     <div className="flex gap-3">
-      <div className="text-base font-bold text-shop-dark-green">{price}</div>
-      <div className="line-through text-gray-500">{priceAfterDiscount}</div>
+      <div className=" font-bold text-shop-dark-green text-sm md:text-base">
+        {price}
+      </div>
+      <div className="line-through text-gray-500 text-sm md:text-base">
+        {priceAfterDiscount}
+      </div>
     </div>
   );
 }
