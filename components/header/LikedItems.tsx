8104dad851520379a3c00eb5d5b51ee29b5ffafa
Mohamed ashraf <muhamed77700m@gmail.com>
2025-09-29 01:47:06 +0300

    add favorite & cart logic

--- components/header/LikedItems.tsx
+++ components/header/LikedItems.tsx
@@ -12,7 +12,7 @@ function LikedItems() {
         className="text-lightColor hover:text-shop-light-green hoverEffect"
       />
       <span className="absolute -top-1 -right-1 bg-shop-dark-green flex justify-center items-center w-3.5 h-3.5 rounded-full text-white text-xs font-bold ">
-        {favoriteProduct.length - 1}
+        {favoriteProduct.length}
       </span>
     </div>
   );
